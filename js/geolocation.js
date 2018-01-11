3ab9e6abf92bafa2ce55729751b8b92ce6c225dd
Nikitabolude <32522791+Nikitabolude@users.noreply.github.com>
2018-01-11 12:37:29 +0300

    imit commit0

---
+++ js/geolocation.js
@@ -0,0 +1,34 @@
+﻿ navigator.geolocation.getCurrentPosition(updatePosition);
+
+        function updatePosition(pos) {
+            // задание координат.
+            var position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+
+            // параметры для карты.
+            var options = {
+                zoom: 14,
+                center: position, // позиция на карте
+                mapTypeId: google.maps.MapTypeId.ROADMAP // тип карты - ROADMAP, SATELLITE, HYBRID and TERRAIN
+            };
+
+            // объект карты.
+            var map = new google.maps.Map(document.getElementById("map"), options);
+
+            // маркер на карте
+            var marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                title: "Это Вы!"
+            });
+
+            // объект всплывающей подсказки.
+            var infowindow = new google.maps.InfoWindow({
+                content: "Скорее всего, это Ваше текущее положение"
+            });
+
+            // присвоение обработчика на нажатие по маркеру.
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+        }
